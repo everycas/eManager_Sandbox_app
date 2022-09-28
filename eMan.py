ea58c8cbbcde831659f059aa6d12bca84eba6951
Admin <everycas@gmail.com>
2022-09-29 02:17:23 +0400

    modify eMan_logic.txt, eMan.py

--- eMan.py
+++ eMan.py
@@ -12,8 +12,7 @@ CORRS_COL = 'corrs'
 ORDERS_COL = 'orders'
 BILLS_COL = 'bills'
 
-
-# ini
+# ini obj
 INI = Ini()
 INI.ini_name = INI_NAME
 INI.log_name = LOG_NAME
@@ -33,91 +32,69 @@ def connect_to_server():
     return pymongo.MongoClient(ini_mongo_connection_string)
 
 
-def create_new_base(connection):
+def create_select_base(connection):
 
-    """ Create new eMan base from pattern """
+    """ Create / select eMan base """
 
-    # Create / select db
-    db = connection[DB_NAME]
-    # create collections
-    sets = db[SETS_COL]
-    items = db[ITEMS_COL]
-    corrs = db[CORRS_COL]
-    orders = db[ORDERS_COL]
-    bills = db[BILLS_COL]
-    # create docs
-    sets_doc = {'lang': 'eng'}
-    items_doc = {'name': 'Пирожок с картошкой', 'price': 120.95}
-    corrs_doc = {'name': 'Admin', 'role': 'admin', 'type': 'staff'}
-    orders_doc = {'data': dt.datetime.now(), 'number': ''}
-    bills_doc = {'data': dt.datetime.now(), 'number': ''}
-
-    # insert docs to collections
-    sets.insert_one(sets_doc)
-    items.insert_one(items_doc)
-    corrs.insert_one(corrs_doc)
-    orders.insert_one(orders_doc)
-    bills.insert_one(bills_doc)
+    return connection[DB_NAME]
 
 
-def doc_insert(connection, collection, document):
+def new_doc_insert(connection, collection: str, document: dict):
 
     """ Insert new document to data collection """
 
-    client = connection
-    db = client[DB_NAME]
+    db = connection[DB_NAME]
     col = db[collection]
     doc = document
     col.insert_one(doc)
 
 
+def find_sort_docs(connection, collection: str, search_request: str):
+
+    """ Find all docs in selected collection that is fits by search request """
+
+    pass
+
+
 def to_guid(num_string: str):
 
-    """ Convert any number string to guid_code SH5 """
+    """ Convert any string number to guid string """
 
-    zero_add = ''
-    guid_mask = '{00000000-0000-0000-0000-'
-    postfix = '}'
+    add_zero = ''
+    guid_prefix = '{00000000-0000-0000-0000-'
+    guid_postfix = '}'
 
     if len(num_string) < 12:
         diff = 12 - len(num_string)  # 6
         for _ in range(diff):
-            zero_add += '0'
-            code = zero_add + num_string
+            add_zero += '0'
+            code = add_zero + num_string
     elif len(num_string) > 12:
         diff = len(num_string) - 12
         code = num_string[diff:]
     else:
         code = num_string
 
-    result = guid_mask + code + postfix
-    return result
+    result_guid = guid_prefix + code + guid_postfix
+
+    return result_guid
 
 
-def dish_doc(guid: str, name: str, group: str, price: float, qnt: float):
-
-    """ Template - newDish """
-
-    dish = {
-
-        'guid': guid,           # dish guid / use to_guid def
-        'name': name,           # dish name
-        'type': 'Dish',         # item type
-        'group': group,         # dish group / parent
-        'munit': 'Portion',     # dish measure unit
-        'qnt': qnt,             # dish quantity
-        'price': price          # dish price
-
+SAMPLE_DOC_DISH_ITEM = {
+    'guid': to_guid(num_string='1'),
+    'type': 'Dish',
+    'group': 'Meat',
+    'name': 'Beefsteak',
+    'munit': 'Portion',
+    'qnt': 1.0,
+    'price': 120.45
     }
 
-    return dish
+client = connect_to_server()
 
+create_select_base(connection=client)
 
-c = connect_to_server()
-# create_new_base(connection=c)
-
-new_dish = dish_doc(guid='', name='Beaf Steak', group='Meat', price=120.45, qnt=1.0)
-doc_insert(connection=c, collection='items', document=new_dish)
+new_doc_insert(connection=client, collection=ITEMS_COL, document=SAMPLE_DOC_DISH_ITEM)
 
 
 
