e711849fe0c00dc8d5c060e0a6cf6043767baab2
Administrator <everycas@gmail.com>
2023-01-29 03:11:54 +0400

    Update eMan.py

--- eMan.py
+++ eMan.py
@@ -3,120 +3,31 @@ import datetime as dt
 import uuid
 import pymongo
 
+# ini & log names
 INI_NAME = 'eMan.ini'
 LOG_NAME = 'eMan.log'
-DT_NOW = dt.datetime.now()
-DT_STRING = ''.join([char for char in str(dt.datetime.now()) if char.isnumeric()])[: 20]  # 'yyyymmddhhmmssms'
-
-DB_NAME = 'USER-10000001-DB'  # every new user (owner / admin) will get own new base
-SETS_COL = 'settings'
-ITEMS_COL = 'items'
-CORRS_COL = 'corrs'
-ORDERS_COL = 'orders'
-BILLS_COL = 'bills'
 
 # ini obj
 INI = Ini()
 INI.ini_name = INI_NAME
 INI.log_name = LOG_NAME
 
+# db
+INI_DBNAME = INI.get(section='base', param='name')  # dbname from ini
+NEW_DBNAME = 'USER-10000001-DB'  # every new user (owner / admin) will get own new base
 
-def connect_to_server():
+DT_NOW = dt.datetime.now()
+DT_STRING = ''.join([char for char in str(dt.datetime.now()) if char.isnumeric()])[: 20]  # 'yyyymmddhhmmssms'
 
-    """ Make client connection to MongoDB Sever / MongoClient('connection string') """
+# default db collections
+SETS_COL = 'settings'
+ITEMS_COL = 'items'
+CORRS_COL = 'corrs'
+ORDERS_COL = 'orders'
+BILLS_COL = 'bills'
 
-    # connection string params
-    ini_server_name = INI.get(section='server', param='name')
-    ini_server_ip = INI.get(section='server', param='ip')
-    ini_server_port = INI.get(section='server', param='port')
-
-    # connection string
-    ini_mongo_connection_string = f"{ini_server_name}://{ini_server_ip}:{ini_server_port}/"
-
-    connection = pymongo.MongoClient(ini_mongo_connection_string)
-    return connection
-
-
-def create_or_select_base(connection: object):
-
-    """ Create / select eMan base """
-
-    return connection[DB_NAME]
-
-
-def insert_one_doc_to_col(connection: object, collection: str, document: dict):
-
-    """ Insert new document to data collection / Mongo insert_one({}) """
-
-    db = connection[DB_NAME]
-    col = db[collection]
-    doc = document
-    col.insert_one(doc)
-
-
-def insert_many_docs_to_col(connection: object, collection: str, docs_list: list):
-
-    """ Insert new documents to data collection / Mongo insert_many([{}, ... {}]) """
-
-    db = connection[DB_NAME]
-    col = db[collection]
-    col.insert_many(docs_list)
-
-
-def find_docs_in_col(connection: object, collection: str, d_key: str, d_value: str):
-
-    """ Find documents in collection by search request / Mongo find({d_key: d_value})  """
-
-    db = connection[DB_NAME]
-    col = db[collection]
-
-    return [doc for doc in col.find({d_key: d_value})]
-
-
-def find_all_docs_in_col(connection: object, collection: str):
-
-    """ Find all docs in selected collection / Mongo find() """
-
-    db = connection[DB_NAME]
-    col = db[collection]
-
-    return [doc for doc in col.find()]
-
-
-def del_one_doc_from_col(connection: object, collection: str, d_key: str, d_value: 'str'):
-
-    """ Delete one document in collection / Mongo delete_one({d_key: d_value}) """
-
-    db = connection[DB_NAME]
-    col = db[collection]
-    col.delete_one({d_key: d_value})
-
-
-def del_many_docs_from_col(connection: object, collection: str, d_key: str, d_value: str):
-
-    """ Delete all docs in collection filtered by search request / Mongo deleteMany({d_key: d_value}) """
-
-    db = connection[DB_NAME]
-    col = db[collection]
-    col.delete_many({d_key: d_value})
-
-
-def del_all_docs_from_col(connection: object, collection: str):
-
-    """ Delete all documents from selected collection / Mongo drop() """
-
-    db = connection[DB_NAME]
-    col = db[collection]
-    col.drop()
-
-
-def update_one_doc_in_col(connection: object, collection: str, d_key: str, d_value: str, update: dict):
-
-    """ Update one document in collection / Mongo update_one({d_key: d_value}, {'$set': {update}}) """
-
-    db = connection[DB_NAME]
-    col = db[collection]
-    col.update_one({d_key: d_value}, {'$set': update})
+# MONGO API -------------------->
+# GENERATORS ---------->
 
 
 def dbname_generator(connection: object):
@@ -135,7 +46,7 @@ def uid_generator(connection: object, collection: str):
 
     """ Documents id generator """
 
-    db = connection[DB_NAME]
+    db = connection[NEW_DBNAME]
     col = db[collection]
     ids = [doc['_id'] for doc in col.find()]
 
@@ -171,6 +82,137 @@ def guid_generator(num_string: str):
     result_guid = guid_prefix + guid_yyyymmdd + guid_hhmm + guid_ssms + guid_uuid + code + guid_postfix
 
     return result_guid
+
+
+# MONGO SERVER & BASE -------------->
+
+
+def connect_to_server():
+
+    """ Make client connection to MongoDB Sever / MongoClient('connection string') """
+
+    # connection string params
+    ini_server_name = INI.get(section='server', param='name')
+    ini_server_ip = INI.get(section='server', param='ip')
+    ini_server_port = INI.get(section='server', param='port')
+
+    # connection string
+    ini_mongo_connection_string = f"{ini_server_name}://{ini_server_ip}:{ini_server_port}/"
+
+    try:
+        connection = pymongo.MongoClient(ini_mongo_connection_string)
+
+    # connection error logging
+    except Exception as Argument:
+        with open(LOG_NAME, "a") as log_file:
+            log_file.write(f"\n{DT_NOW}: mongo server connection error. Message:{str(Argument)}")
+    else:
+        return connection
+
+
+def create_base(connection: object):
+
+    """ Create new eMan db if ini[base]name not specified """
+
+    server = connect_to_server()
+    new_dbname = dbname_generator(connection=server)
+
+    try:
+        if INI_DBNAME == '':
+            return connection[new_dbname]
+
+    except Exception as Argument:
+        with open(LOG_NAME, "a") as log_file:
+            log_file.write(f"\n{DT_NOW}: mongo server db creation error. Message:{str(Argument)}")
+
+    else:
+        pass
+
+
+def select_base():
+    """ Get eMan base from ini """
+
+    # get dbname from ini
+    ini_dbname = INI.get(section='base', param='name')
+
+    # if dbname not in ini rise error in log
+
+
+
+
+def insert_one_doc_to_col(connection: object, collection: str, document: dict):
+
+    """ Insert new document to data collection / Mongo insert_one({}) """
+
+    db = connection[NEW_DBNAME]
+    col = db[collection]
+    doc = document
+    col.insert_one(doc)
+
+
+def insert_many_docs_to_col(connection: object, collection: str, docs_list: list):
+
+    """ Insert new documents to data collection / Mongo insert_many([{}, ... {}]) """
+
+    db = connection[NEW_DBNAME]
+    col = db[collection]
+    col.insert_many(docs_list)
+
+
+def find_docs_in_col(connection: object, collection: str, d_key: str, d_value: str):
+
+    """ Find documents in collection by search request / Mongo find({d_key: d_value})  """
+
+    db = connection[NEW_DBNAME]
+    col = db[collection]
+
+    return [doc for doc in col.find({d_key: d_value})]
+
+
+def find_all_docs_in_col(connection: object, collection: str):
+
+    """ Find all docs in selected collection / Mongo find() """
+
+    db = connection[NEW_DBNAME]
+    col = db[collection]
+
+    return [doc for doc in col.find()]
+
+
+def del_one_doc_from_col(connection: object, collection: str, d_key: str, d_value: 'str'):
+
+    """ Delete one document in collection / Mongo delete_one({d_key: d_value}) """
+
+    db = connection[NEW_DBNAME]
+    col = db[collection]
+    col.delete_one({d_key: d_value})
+
+
+def del_many_docs_from_col(connection: object, collection: str, d_key: str, d_value: str):
+
+    """ Delete all docs in collection filtered by search request / Mongo deleteMany({d_key: d_value}) """
+
+    db = connection[NEW_DBNAME]
+    col = db[collection]
+    col.delete_many({d_key: d_value})
+
+
+def del_all_docs_from_col(connection: object, collection: str):
+
+    """ Delete all documents from selected collection / Mongo drop() """
+
+    db = connection[NEW_DBNAME]
+    col = db[collection]
+    col.drop()
+
+
+def update_one_doc_in_col(connection: object, collection: str, d_key: str, d_value: str, update: dict):
+
+    """ Update one document in collection / Mongo update_one({d_key: d_value}, {'$set': {update}}) """
+
+    db = connection[NEW_DBNAME]
+    col = db[collection]
+    col.update_one({d_key: d_value}, {'$set': update})
 
 
 # TESTS ------------------------------------------------- #
